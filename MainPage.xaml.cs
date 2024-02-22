9b34d6e17c478ec5be17c4c340f38e899eeba389
J-Wales <j.wales@edu.sait.ca>
2024-02-22 10:53:35 -0700

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace EventDrivenAppsAndExceptionHandling
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
